56f4c020465e0b4d8357e5b731d1d303ffebc350
ext.zhangjunpeng5 <ext.zhangjunpeng5@jd.com>
2025-04-10 18:32:21 +0800

    feat: add env

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import viteImagemin from 'vite-plugin-imagemin'
@@ -7,103 +7,111 @@ import stylelint from 'vite-plugin-stylelint'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { dayjs } from 'element-plus'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    viteImagemin({
-      gifsicle: {
-        optimizationLevel: 7,
-        interlaced: false,
+export default defineConfig(({ mode }) => {
+  const version = dayjs().format('YYYYMMDDHHmm')
+  const folder = `aaa/${mode}/${version}`
+  const publicDir = `https://aliyun/pro/${folder}` // 发布远程地址
+  const config = loadEnv(mode, process.cwd()) // 加载环境变量
+  console.log('config', mode, config)
+  return {
+    base: mode === 'production' ? publicDir : '/',
+    plugins: [
+      vue(),
+      vueDevTools(),
+      viteImagemin({
+        gifsicle: {
+          optimizationLevel: 7,
+          interlaced: false,
+        },
+        optipng: {
+          optimizationLevel: 7,
+        },
+        mozjpeg: {
+          quality: 80,
+        },
+        pngquant: {
+          quality: [0.8, 0.9],
+          speed: 4,
+        },
+        svgo: {
+          plugins: [
+            {
+              name: 'removeViewBox',
+            },
+            {
+              name: 'removeEmptyAttrs',
+              active: false,
+            },
+          ],
+        },
+        webp: {
+          quality: 80,
+        },
+      }),
+      stylelint({
+        fix: true,
+        include: ['src/**/*.{vue,less,css}'],
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
       },
-      optipng: {
-        optimizationLevel: 7,
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          javascriptEnabled: true,
+          // 如果需要全局变量，可以在这里添加
+          // additionalData: `@import "@/assets/styles/variables.less";`
+        },
       },
-      mozjpeg: {
-        quality: 80,
-      },
-      pngquant: {
-        quality: [0.8, 0.9],
-        speed: 4,
-      },
-      svgo: {
+      // 确保 PostCSS 配置被应用
+      postcss: {
         plugins: [
-          {
-            name: 'removeViewBox',
-          },
-          {
-            name: 'removeEmptyAttrs',
-            active: false,
-          },
+          // 这里可以是空的，因为我们已经在 postcss.config.js 中配置了插件
         ],
       },
-      webp: {
-        quality: 80,
-      },
-    }),
-    stylelint({
-      fix: true,
-      include: ['src/**/*.{vue,less,css}'],
-    }),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
     },
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        // 如果需要全局变量，可以在这里添加
-        // additionalData: `@import "@/assets/styles/variables.less";`
-      },
+    // 图片资源优化配置
+    build: {
+      assetsInlineLimit: 4096, // 小于4kb的图片会被base64内联
+      // rollupOptions: {
+      //   output: {
+      //     // 自定义图片输出目录
+      //     assetFileNames: (assetInfo) => {
+      //       if (!assetInfo.name) return 'assets/[name]-[hash][extname]'
+      //       const info = assetInfo.name.split('.')
+      //       let extType = info[info.length - 1]
+      //       if (/\.(png|jpe?g|gif|svg|webp|ico)(\?.*)?$/.test(assetInfo.name)) {
+      //         extType = 'img'
+      //       }
+      //       return `assets/${extType}/[name]-[hash][extname]`
+      //     },
+      //     // 自定义 chunk 输出目录
+      //     chunkFileNames: 'assets/js/[name]-[hash].js',
+      //     entryFileNames: 'assets/js/[name]-[hash].js'
+      //   }
+      // }
     },
-    // 确保 PostCSS 配置被应用
-    postcss: {
-      plugins: [
-        // 这里可以是空的，因为我们已经在 postcss.config.js 中配置了插件
-      ],
-    },
-  },
-  // 图片资源优化配置
-  build: {
-    assetsInlineLimit: 4096, // 小于4kb的图片会被base64内联
-    // rollupOptions: {
-    //   output: {
-    //     // 自定义图片输出目录
-    //     assetFileNames: (assetInfo) => {
-    //       if (!assetInfo.name) return 'assets/[name]-[hash][extname]'
-    //       const info = assetInfo.name.split('.')
-    //       let extType = info[info.length - 1]
-    //       if (/\.(png|jpe?g|gif|svg|webp|ico)(\?.*)?$/.test(assetInfo.name)) {
-    //         extType = 'img'
-    //       }
-    //       return `assets/${extType}/[name]-[hash][extname]`
-    //     },
-    //     // 自定义 chunk 输出目录
-    //     chunkFileNames: 'assets/js/[name]-[hash].js',
-    //     entryFileNames: 'assets/js/[name]-[hash].js'
-    //   }
-    // }
-  },
-  server: {
-    host: '0.0.0.0',
-    proxy: {
-      '/api': {
-        target: 'http://163.22.55.255',
-        changeOrigin: true,
-        secure: true,
-        rewrite: (path) => path.replace(/\/api/, ''),
+    server: {
+      host: '0.0.0.0',
+      proxy: {
+        '/api': {
+          target: config.VITE_API_SERVER,
+          changeOrigin: true,
+          secure: true,
+          rewrite: (path) => path.replace(/\/api/, ''),
+        },
       },
     },
-  },
+  }
 })
